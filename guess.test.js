02ef3ffd2869ae787b3f1dbaf5a00e58d797f166
Ricky Pai <rickyp999+github@gmail.com>
2021-03-30 12:55:08 -0700

    test description

--- guess.test.js
+++ guess.test.js
@@ -1,8 +1,10 @@
 import { checkGuess } from './guess';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(checkGuess(['G', 'Y', 'G', 'Y'], ['G', 'Y', 'G', 'Y'])).toStrictEqual({
-    rightGuessRightPosition: 4,
-    rightGuessWrongPosition: 0,
+describe('checkGuess', () => {
+  test('all correct code and correct order', () => {
+    expect(checkGuess(['G', 'Y', 'G', 'Y'], ['G', 'Y', 'G', 'Y'])).toStrictEqual({
+      rightGuessRightPosition: 4,
+      rightGuessWrongPosition: 0,
+    });
   });
 });
